a3a4938019be6c85cb5d75ab71edfd408068b072
Prachi5791 <patilmg2009@gmail.com>
2024-06-30 12:31:29 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/your-subdirectory/', // Set to your specific subdirectory if needed
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  }
+});
+
