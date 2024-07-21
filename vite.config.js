e8e434ea9f0f089e4fb002e0587eecb623407f8a
Prachi5791 <patilmg2009@gmail.com>
2024-07-21 19:46:30 +0530

    vite change

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/your-subdirectory/', // Set to your specific subdirectory if needed
+  base: '/Gemini_clone_project/', // Set to your specific subdirectory if needed
   plugins: [react()],
   build: {
     outDir: 'dist',
